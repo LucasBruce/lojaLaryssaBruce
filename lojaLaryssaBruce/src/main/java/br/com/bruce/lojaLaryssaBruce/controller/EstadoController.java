006bbbc22f88eeecab0a340b78796ba2b2886096
lucas.matheus <lucas.matheus@N2018004595.pb.prf>
2021-05-24 14:02:24 -0300

    lojaLaryssa

--- lojaLaryssaBruce/src/main/java/br/com/bruce/lojaLaryssaBruce/controller/EstadoController.java
+++ lojaLaryssaBruce/src/main/java/br/com/bruce/lojaLaryssaBruce/controller/EstadoController.java
@@ -2,9 +2,12 @@ package br.com.bruce.lojaLaryssaBruce.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.method.support.ModelAndViewContainer;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.bruce.lojaLaryssaBruce.modelo.Estado;
 import br.com.bruce.lojaLaryssaBruce.repositorio.EstadoRepositorio;
 
 @Controller
@@ -13,6 +16,12 @@ public class EstadoController {
 	@Autowired
 	private EstadoRepositorio estadoRepositorio;
 	
-	public ModelAndView 
-
+	@GetMapping("/gerencia")
+	public ModelAndView cadastrar(Estado estado) {
+		ModelAndView mav = new ModelAndView("/gerencia/estado/cadastrar");
+		mav.addObject("estado", estado);
+		return mav;
+	}
+	
+	
 }
