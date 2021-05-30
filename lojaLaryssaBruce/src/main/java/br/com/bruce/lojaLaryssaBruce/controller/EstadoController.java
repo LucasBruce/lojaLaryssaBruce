e85825b7a38b4d129d2de379c49a6ea80a86bf8c
lucas <lucas@lucas-aspiref5573>
2021-05-30 10:40:57 -0300

    lojaLaryssa v1.0

--- lojaLaryssaBruce/src/main/java/br/com/bruce/lojaLaryssaBruce/controller/EstadoController.java
+++ lojaLaryssaBruce/src/main/java/br/com/bruce/lojaLaryssaBruce/controller/EstadoController.java
@@ -1,8 +1,13 @@
 package br.com.bruce.lojaLaryssaBruce.controller;
 
+import javax.naming.Binding;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.bruce.lojaLaryssaBruce.modelo.Estado;
@@ -25,6 +30,15 @@ public class EstadoController {
 		ModelAndView mav = new ModelAndView("/gerencia/estado/cadastro");
     	mav.addObject("estado", estado);
     	return mav;
+	}
+	
+	@PostMapping("gerencia/estado/salvar")
+	public ModelAndView salvar(@Valid Estado estado, BindingResult result) {
+		if(result.hasErrors()) {
+			return cadastro(estado);
+		}
+		this.estadoRepositorio.saveAndFlush(estado);
+		return cadastro(new Estado());
 	}
 	
 	
