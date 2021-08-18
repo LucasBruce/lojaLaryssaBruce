39cdb3fc70e38e5a9cb1efc7f8fcb1abdda0b220
LucasBruce <lucasmatheusbruce@gmail.com>
2021-08-18 15:50:26 -0300

    lojaLaryssa v2.0

--- lojaLaryssaBruce/src/main/java/br/com/bruce/lojaLaryssaBruce/controller/IndexController.java
+++ lojaLaryssaBruce/src/main/java/br/com/bruce/lojaLaryssaBruce/controller/IndexController.java
@@ -1,15 +1,35 @@
 package br.com.bruce.lojaLaryssaBruce.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
+
+import br.com.bruce.lojaLaryssaBruce.modelo.Produto;
+import br.com.bruce.lojaLaryssaBruce.service.ProdutoService;
 
 @Controller
 public class IndexController {
 
+@Autowired
+private ProdutoService produtoService;
+
 	@GetMapping("/")
 	public ModelAndView index() {
 		ModelAndView mav = new ModelAndView("index");
+		List<Produto> produto = this.produtoService.findAll();
+		mav.addObject("listaProdutos", produto);
+		return mav;
+	}
+	
+	@GetMapping("/produto/{id}")
+	public ModelAndView getDetailsProduto(@PathVariable("id") long id) {
+		ModelAndView mav = new ModelAndView("produtoDetalhe");
+		Produto produto = this.produtoService.findById(id);
+		mav.addObject("produto",produto);
 		return mav;
 	}
 }
